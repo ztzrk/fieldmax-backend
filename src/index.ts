f2f967965c8e480ccfe7c556809a563996869e52
ztzrk <ralif877@gmail.com>
2025-06-14 21:33:09 +0800

    feat: Add CORS and cookie-parser middleware to the application

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,20 @@ import express, { Express, Request, Response } from "express";
 import { AuthRoute } from "./auth/auth.route";
 import { UsersRoute } from "./users/users.route";
 
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(
+    cors({
+        origin: "http://localhost:3001", // Adjust this to your frontend URL
+        credentials: true, // Allow cookies to be sent
+    })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 const authRoute = new AuthRoute();
