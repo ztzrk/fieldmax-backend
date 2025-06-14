12578f2c8dd17c9b80f3f008a7108b6553836b2e
ztzrk <ralif877@gmail.com>
2025-06-14 21:33:04 +0800

    feat: Add Session model and migration for sessions table

---
+++ prisma/migrations/20250614081609_add_sessions_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
