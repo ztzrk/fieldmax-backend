03d1c38712e6694a9654d5a1404eba86febf5162
ztzrk <ralif877@gmail.com>
2025-07-02 18:08:27 +0700

    feat: Implement SportTypesController with CRUD operations for sport types

---
+++ src/sport-types/sport-types.controller.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+import { SportTypesService } from "./sport-types.service";
+import { CreateSportTypeDto } from "./dtos/create-sport-type.dto";
+
+export class SportTypesController {
+    public service = new SportTypesService();
+
+    public getAll = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const data = await this.service.findAll();
+            res.status(200).json({ data, message: "findAll" });
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    public create = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const sportTypeData: CreateSportTypeDto = req.body;
+            const data = await this.service.create(sportTypeData);
+            res.status(201).json({ data, message: "created" });
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    public update = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { id } = req.params;
+            const sportTypeData: CreateSportTypeDto = req.body;
+            const data = await this.service.update(id, sportTypeData);
+            res.status(200).json({ data, message: "updated" });
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    public delete = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { id } = req.params;
+            const data = await this.service.delete(id);
+            res.status(200).json({ data, message: "deleted" });
+        } catch (error) {
+            next(error);
+        }
+    };
+}
