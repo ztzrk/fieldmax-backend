194cc2191e1d17ac490a9d0b1470f54a93a4148e
ztzrk <ralif877@gmail.com>
2025-07-05 19:58:21 +0700

    feat: Add adminOnlyMiddleware to restrict access to admin users

---
+++ src/middleware/admin.middleware.ts
@@ -0,0 +1,13 @@
+import { Router, Response, NextFunction, Request } from "express";
+
+export const adminOnlyMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (req.user && req.user.role === "ADMIN") {
+        next();
+    } else {
+        res.status(403).json({ message: "Forbidden: Requires Admin role" });
+    }
+};
