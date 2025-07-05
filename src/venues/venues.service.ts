8aab3099c268d4bc359bc4b3f24ba6cfd1e4b2c8
ztzrk <ralif877@gmail.com>
2025-07-05 19:58:44 +0700

    feat: Implement VenuesService to manage venue operations

---
+++ src/venues/venues.service.ts
@@ -0,0 +1,58 @@
+import prisma from "../db";
+import { CreateVenueDto, UpdateVenueDto } from "./dtos/venue.dto";
+
+export class VenuesService {
+    public async findAll() {
+        const venues = await prisma.venue.findMany({
+            include: {
+                renter: {
+                    select: {
+                        fullName: true,
+                        email: true,
+                    },
+                },
+                _count: {
+                    select: { fields: true },
+                },
+            },
+        });
+        return venues;
+    }
+
+    public async findById(id: string) {
+        const venue = await prisma.venue.findUnique({
+            where: { id },
+            include: {
+                fields: true,
+                renter: true,
+            },
+        });
+        if (!venue) throw new Error("Venue not found");
+        return venue;
+    }
+
+    public async create(data: CreateVenueDto) {
+        const renter = await prisma.user.findUnique({
+            where: { id: data.renterId },
+        });
+        if (!renter || renter.role !== "RENTER") {
+            throw new Error("Invalid renter ID provided.");
+        }
+
+        const newVenue = await prisma.venue.create({ data });
+        return newVenue;
+    }
+
+    public async update(id: string, data: UpdateVenueDto) {
+        const updatedVenue = await prisma.venue.update({
+            where: { id },
+            data,
+        });
+        return updatedVenue;
+    }
+
+    public async delete(id: string) {
+        const deletedVenue = await prisma.venue.delete({ where: { id } });
+        return deletedVenue;
+    }
+}
