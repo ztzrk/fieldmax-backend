8297419a33a1ada1eac681a0d9ff621a9cbeed18
ztzrk <ralif877@gmail.com>
2025-07-08 13:20:59 +0700

    feat: Add methods for managing schedule overrides in FieldsController

--- src/fields/fields.controller.ts
+++ src/fields/fields.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { FieldsService } from "./fields.service";
 import { CreateFieldDto } from "./dtos/create-field.dto";
+import { ScheduleOverrideDto } from "./dtos/override.dto";
 
 export class FieldsController {
     public service = new FieldsService();
@@ -68,6 +69,51 @@ export class FieldsController {
             const { ids } = req.body;
             const data = await this.service.deleteMultiple(ids);
             res.status(200).json({ data, message: "deleted multiple" });
+        } catch (error) {
+            next(error);
+        }
+    };
+    public getOverrides = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        try {
+            const { fieldId } = req.params;
+            const data = await this.service.getOverrides(fieldId);
+            res.status(200).json({ data });
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    public createOverride = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        try {
+            const { fieldId } = req.params;
+            const overrideData: ScheduleOverrideDto = req.body;
+            const data = await this.service.createOverride(
+                fieldId,
+                overrideData
+            );
+            res.status(201).json({ data, message: "created" });
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    public deleteOverride = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        try {
+            const { overrideId } = req.params;
+            const data = await this.service.deleteOverride(overrideId);
+            res.status(200).json({ data, message: "deleted" });
         } catch (error) {
             next(error);
         }
