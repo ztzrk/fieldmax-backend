db0e410e32bdb21f834c43144efb6b2b46a620dc
ztzrk <ralif877@gmail.com>
2025-08-23 20:36:12 +0700

    feat: Add PaymentsRoute to the application for payment processing

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import { RenterRoute } from "./renter/renter.route";
 import { ChatRoute } from "./chat/chat.route";
 import { ProfileRoute } from "./profile/profile.route";
 import { BookingsRoute } from "./bookings/bookings.route";
+import { PaymentsRoute } from "./payments/payments.route";
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
@@ -38,6 +39,7 @@ const renterRoute = new RenterRoute();
 const chatRoute = new ChatRoute();
 const profileRoute = new ProfileRoute();
 const bookingsRoute = new BookingsRoute();
+const paymentsRoute = new PaymentsRoute();
 
 app.use("/api", authRoute.router);
 app.use("/api", usersRoute.router);
@@ -49,6 +51,7 @@ app.use("/api", renterRoute.router);
 app.use("/api", chatRoute.router);
 app.use("/api", profileRoute.router);
 app.use("/api", bookingsRoute.router);
+app.use("/api", paymentsRoute.router);
 
 // Simple test route
 app.get("/", (req: Request, res: Response) => {
