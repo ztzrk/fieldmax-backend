606799f9b5fb48f9b6cba795103027263fa76c63
ztzrk <ralif877@gmail.com>
2025-08-24 19:11:44 +0700

    feat: Add PaginationDto for handling pagination parameters

---
+++ src/dtos/pagination.dto.ts
@@ -0,0 +1,21 @@
+import { Type } from "class-transformer";
+import { IsInt, IsOptional, IsString, Max, Min } from "class-validator";
+
+export class PaginationDto {
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    page?: number = 1;
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    @Max(100)
+    limit?: number = 10;
+
+    @IsOptional()
+    @IsString()
+    search?: string;
+}
