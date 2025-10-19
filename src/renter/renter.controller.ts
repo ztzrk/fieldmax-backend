cb557227fbeaf0a9e4a61d7113a5798dcf08117a
ztzrk <ralif877@gmail.com>
2025-10-19 20:26:54 +0700

    feat: Add getMyFieldsWithPagination method for paginated field retrieval

--- src/renter/renter.controller.ts
+++ src/renter/renter.controller.ts
@@ -204,6 +204,23 @@ export class RenterController {
         }
     };
 
+    public getMyFieldsWithPagination = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        try {
+            const renterId = req.user!.id;
+            const data = await this.service.findMyFieldsWithPagination(
+                renterId,
+                req.query
+            );
+            res.status(200).json({ data });
+        } catch (error) {
+            next(error);
+        }
+    };
+
     public getMyBookingsWithPagination = async (
         req: Request,
         res: Response,
